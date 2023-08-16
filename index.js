b16271cddd00f1450884c7d2df80b1bc2c0c6b27
foxstudent105381 <maksymovychmail@gmail.com>
2023-08-16 16:33:11 +0300

    add some consoles

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const fs = require('fs')
+const MongoClient = require('mongodb').MongoClient
 
 const app = express()
 const PORT = process.env.PORT || 3000
+const MONGO_URI =
+	'mongodb+srv://hixstore:4dFeK1zbgQGSHPZY@cluster0.rafsdqy.mongodb.net/?retryWrites=true&w=majority' // Замените на свою строку подключения
+
+let db
+let productsCollection
 
 app.use(bodyParser.json())
 
+// Настройка соединения с MongoDB
+MongoClient.connect(MONGO_URI, { useUnifiedTopology: true }, (err, client) => {
+	if (err) throw err
+	db = client.db('YOUR_DB_NAME') // Замените на имя вашей базы данных
+	productsCollection = db.collection('products')
+})
+
 app.post('/api/addProduct', (req, res) => {
-	// Получите данные товара из req.body
 	const productData = req.body
-
-	// Прочитайте файл JSON, обновите его и перезапишите
-	const products = JSON.parse(fs.readFileSync('products.json', 'utf-8'))
-	products.push(productData)
-	fs.writeFileSync('products.json', JSON.stringify(products, null, 2))
-
-	res.status(201).json({ message: 'Product added successfully' })
+	productsCollection.insertOne(productData, (err, result) => {
+		if (err) throw err
+		res.status(201).json({ message: 'Product added successfully' })
+	})
 })
 
 app.delete('/api/deleteProduct/:code', (req, res) => {
 	const codeToDelete = req.params.code
-
-	// Прочитайте файл JSON, найдите и удалите товар
-	const products = JSON.parse(fs.readFileSync('products.json', 'utf-8'))
-	const updatedProducts = products.filter(
-		product => product.code !== codeToDelete
-	)
-	fs.writeFileSync('products.json', JSON.stringify(updatedProducts, null, 2))
-
-	res.json({ message: 'Product deleted successfully' })
+	productsCollection.deleteOne({ code: codeToDelete }, (err, result) => {
+		if (err) throw err
+		res.json({ message: 'Product deleted successfully' })
+	})
 })
 
 app.listen(PORT, () => {
